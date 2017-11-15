6b7aef934dfb48f3636c6000f5f97601459c5000
Johnny Kru <johnnykru@gmail.com>
2017-11-15 08:55:59 -0500

    updated new remote

--- index.php
+++ index.php
@@ -28,22 +28,22 @@
     <?php include 'navbar.php';?>
 </header>
 	<div class="content_area">
-		<div class="container">
+		<div class="container-fluid">
 		    <div class="row">
-				<div class="welcomTx col-sm-6 col-md-12 col-lg-12">  
-					<div class="kruelIntro col-xs-12 col-sm-12 col-md-12 col-lg-12">					
+				<div class="welcomTx">  
+					<div class="kruelIntro col-xs-12 col-sm-6 col-md-8 col-lg-6 ">					
 					   <h1> About Kruel Militia </h1>
 						<em>Kruel Militia is geared towards active individuals mastering self-development through combat sports, health/fitness and social activities.</em>
 						<em>With personal development comes clarity. Even with an improved sense of direction, there will always be multiple tasks looking and waiting to challenge us all.<em>
 						<em>With these challenges, you will need a team behind you. <em>
 						<em>Take charge now - Join Today!!</em>
 						<br>
-					</div>	
+						<br>
 					
-					<div class="contactForm col-sm-4 col-lg-6 col-md-4 col-xs-4">
+					    
 						 <h1 id ="mailing_title">Have Questions ? </h1><br/>
 						 <form name="contactform" method="post" action="email_form.php">
-						 <table width="450px">
+						 <table  width="450px">
 						 <tr>
 						 <td valign="top col-md-4 col-xs-4">
 						  <label for="first_name">First Name *</label>
@@ -92,14 +92,17 @@
 						</table>
 						</form>
 					</div>
+					<div class="vid_area col-sm-12 col-lg-3 col-lg-offset-1">
+				  <blockquote class="instagram-media col-md-6" data-instgrm-captioned data-instgrm-version="7" style=" position:relavite; top:30px; right:20px; background:#fff; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:458px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:28.194444444444443% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style=" margin:8px 0 0 0; padding:0 4px;"> <a href="https://www.instagram.com/p/BSXRSlAA5US/" style=" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank"></a></p> <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A post shared by Johnny Keokongsy (@johnnykru) on <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2017-04-02T00:54:57+00:00">Apr 1, 2017 at 5:54pm PDT</time></p></div></blockquote> <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>  
+				</div>	
 				</div>		
 			</div>
-		</div>
-	</div>    					       
-
+		</div>  
+	 	
     <div class="footer">
 	<h3 class="contactText"> 919-302-0678 | 4900 NC-55, Durham, NC 27713 </h3><br>
 	</div>
+	</div> 
 	<script src="https://whispering-inlet-81728.herokuapp.com/javascript.js"></script> 	
   </body>
 </html>
